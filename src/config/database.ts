ce649b71da2f915de04fcbb30a818cd4c83d949f
Abraham Arreola <abrahamarrolab198@gmail.com>
2021-03-11 01:41:25 -0600

    backend finished

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,6 +1,16 @@
 import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
 
-export const sequelize = new Sequelize("mysql://root@localhost/interview_db");
+dotenv.config();
+
+const DB_DRIVER = process.env.DATABASE_DRIVER;
+const DB_USER = process.env.DATABASE_USER;
+const DB_URL = process.env.DATABASE_URL;
+const DB_NAME = process.env.DATABASE_NAME;
+
+export const sequelize = new Sequelize(
+    `${DB_DRIVER}://${DB_USER}@${DB_URL}/${DB_NAME}`
+);
 
 sequelize
     .sync()
